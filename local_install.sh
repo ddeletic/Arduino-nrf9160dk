fff2814a19dc3a8d033ab83e44016c320fcebcca
dejan <ddeletic@gmail.com>
2023-09-29 12:38:48 +1000

    Made local install work for any user.

--- local_install.sh
+++ local_install.sh
@@ -4,6 +4,6 @@
 MY_DIR=$(realpath $(dirname $0))
 cd ${MY_DIR}/dist
 
-cp *.json /c/Users/Dejan/AppData/Local/Arduino15
-cp *.tar.gz /c/Users/Dejan/AppData/Local/Arduino15/staging/packages/
+cp *.json /c/Users/${USERNAME}/AppData/Local/Arduino15
+cp *.tar.gz /c/Users/${USERNAME}/AppData/Local/Arduino15/staging/packages/
 
