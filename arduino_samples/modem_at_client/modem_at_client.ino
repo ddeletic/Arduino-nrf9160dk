5233d063b59c6b22bdb024ec0b7730ba534c473c
dejan <ddeletic@gmail.com>
2023-12-03 09:53:36 +1100

    Cosmetics.

--- arduino_samples/modem_at_client/modem_at_client.ino
+++ arduino_samples/modem_at_client/modem_at_client.ino
@@ -1,15 +1,4 @@
-/*
- * Copyright (c) 2018 Nordic Semiconductor ASA
- *
- * SPDX-License-Identifier: LicenseRef-Nordic-5-Clause
- */
-
-#include <zephyr/kernel.h>
-#include <stdio.h>
-#include <string.h>
 #include <modem/nrf_modem_lib.h>
-#include <zephyr/drivers/uart.h>
-#include <zephyr/drivers/clock_control.h>
 #include <zephyr/drivers/clock_control/nrf_clock_control.h>
 #include <Arduino.h>
 
@@ -51,7 +40,6 @@ void setup()
 	int err = nrf_modem_lib_init();
 	if (err) {
 		printk("Modem library initialization failed, error: %d\n", err);
-//		return 0;
 	}
 #endif
 	enable_xtal();
